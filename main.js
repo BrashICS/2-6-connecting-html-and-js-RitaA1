45eec00dbaf1a5ecb2fb0088a58a2ec96626be9f
Rita <rita.anierobi@stu.ocsb.ca>
2024-10-17 14:43:02 +0000

    1

--- main.js
+++ main.js
@@ -18,3 +18,47 @@ function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
+let my_div = document.getElementById("try_me");
+
+console.log(my_div.textContent);
+
+my_div.textContent = "Hi mom!";
+
+document.getElementById("d6").addEventListener("click", roll_d6)
+document.getElementById("d8").addEventListener("click", roll_d8)
+document.getElementById("d6").addEventListener("mouseenter", d6_enter)
+document.getElementById("d6").addEventListener("mouseleave", d6_leave)
+document.getElementById("enter").addEventListener("click", greeting)
+
+
+
+
+function randInt(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function roll_d6() {
+let roll = randInt(1,6)
+document.getElementById("die_roll").textContent = roll
+}
+
+function roll_d8() {
+    let roll = randInt(1,8)
+    document.getElementById("die_roll").textContent = roll
+    }
+
+
+function d6_enter() {
+    document.getElementById("die").textContent = "💝"
+}
+
+function d6_leave() {
+    document.getElementById("die").textContent = "🎲"
+}
+
+
+function greeting() {
+let name = document.getElementById("user_input").value
+document.getElementById("greeting").textContent = `Hello ${name}!`
+}
